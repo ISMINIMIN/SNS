93983a2bde74eaf12d090cdace7b596b03eae19b
ISMINIMIN <summmm.224@gmail.com>
2025-07-01 18:27:56 +0900

    refactor: remove unused field

--- src/main/java/minzdev/sns/model/dto/Alarm.java
+++ src/main/java/minzdev/sns/model/dto/Alarm.java
@@ -2,27 +2,29 @@ package minzdev.sns.model.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import minzdev.sns.model.entity.AlarmArgs;
 import minzdev.sns.model.entity.AlarmEntity;
 import minzdev.sns.model.enumeration.AlarmType;
 
 import java.sql.Timestamp;
 
+@Slf4j
 @Getter
 @AllArgsConstructor
 public class Alarm {
 
     private Integer id;
-    private User user;
     private AlarmType alarmType;
     private AlarmArgs args;
     private Timestamp createdAt;
     private Timestamp deletedAt;
 
     public static Alarm fromEntity(AlarmEntity entity) {
+        // log.info("----- [Alarm] Call fromEntity -----");
+
         return new Alarm(
                 entity.getId(),
-                User.fromEntity(entity.getUser()),
                 entity.getAlarmType(),
                 entity.getArgs(),
                 entity.getCreatedAt(),
